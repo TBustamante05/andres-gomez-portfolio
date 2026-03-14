d4739f7b22044a7e8368312e375a535b36d41bd6
MBustamante <mosquerabustamante88@gmail.com>
2026-03-14 13:27:02 -0500

    Update Home component styles for improved layout and typography

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,6 +1,6 @@
 export const Home = () => {
   return (
-    <div className="h-screen bg-black text-white relative top-10 pt-36 px-4 sm:px-6 md:px-8 lg:px-10 main-title">
+    <div className="min-h-screen lg:h-screen bg-black text-white relative top-10 pt-36 px-4 sm:px-6 md:px-8 lg:px-10 main-title">
       <div className="max-w-7xl w-full mx-auto relative">
         <div className="z-10 relative flex flex-col">
           <div className="flex flex-col lg:flex-row lg:justify-between lg:items-center gap-6 lg:gap-4">
@@ -10,17 +10,17 @@ export const Home = () => {
                 Urban photographer who loves to capture the essence of the city
               </p>
             </div>
-            <h1 className="text-[4rem] sm:text-[6rem] md:text-[10rem] lg:text-[14rem] xl:text-[18rem] font-bold uppercase text-orange-600 z-10 leading-none order-1 lg:order-2 text-center lg:text-left">
+            <h1 className="text-[6rem] sm:text-[10rem] lg:text-[14rem] xl:text-[18rem] font-bold uppercase text-orange-600 z-10 leading-none order-1 lg:order-2 text-left">
               Andrés
             </h1>
           </div>
-          <h1 className="text-[4rem] sm:text-[6rem] md:text-[10rem] lg:text-[14rem] xl:text-[18rem] font-bold uppercase bg-clip-text text-transparent bg-gradient-to-r from-white/30 to-white z-10 leading-none text-center lg:text-left">
+          <h1 className="text-[6rem] sm:text-[10rem] lg:text-[14rem] xl:text-[18rem] font-bold uppercase bg-clip-text text-transparent bg-gradient-to-r from-white/30 to-white z-10 leading-none text-right lg:text-left">
             Gómez
           </h1>
           <img
             src="/andres.jpg"
             alt="Andrés Gómez"
-            className="absolute bottom-0 right-0 w-32 h-24 sm:w-48 sm:h-36 md:w-80 md:h-60 lg:w-96 lg:h-72 xl:w-[600px] xl:h-[450px] object-cover -rotate-12 opacity-70 hidden sm:block"
+            className=" lg:absolute lg:bottom-0 lg:right-0 w-72 lg:h-72 xl:w-[600px] xl:h-[450px] object-cover -rotate-12 lg:opacity-70 z-0 my-14 lg:my-0 p-3 lg:p-0 lg:rounded-none border border-white/30 justify-center items-center mx-auto lg:mx-0"
           />
         </div>
       </div>
